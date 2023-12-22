db034267e80285424935ec35ea03dde7dc22b055
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-12-22 13:11:51 -0600

    Installed NextUI and TailwindCSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         <Providers>
           <main className="text-foreground bg-background">
