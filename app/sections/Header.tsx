2c9e5b444813e2a52729ae6c670cb91328bd6fe0
Cory Ginsberg <cory.ginsberg1@gmail.com>
2023-12-29 17:02:14 -0600

    Updated hero text and pills

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -6,8 +6,7 @@
 
 import {Burger, Container, Drawer, Group, Menu, rem} from '@mantine/core';
 import {useDisclosure} from '@mantine/hooks';
-import {FaGithubSquare} from 'react-icons/fa';
-import {FaLinkedin} from 'react-icons/fa';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
