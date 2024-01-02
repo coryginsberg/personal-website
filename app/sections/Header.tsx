70022e4d8a8974cee82369aca3dfb22050eea3f8
Cory Ginsberg <coryginsberg@meta.com>
2024-01-02 17:06:47 -0600

    Rebase

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -2,6 +2,10 @@
  * Copyright (c) 2023 Cory Ginsberg
  * MIT License
  */
+'use client';
+
+import { Burger, Container, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,22 +13,34 @@ import * as React from 'react';
 
 import RoundedButton from 'components/RoundedButton';
 
-import header from '@styles/header.module.scss';
+import header from '@styles/header.module.css';
+
+const links = [
+  { label: 'GitHub', link: 'https://github.com/coryginsberg' },
+  { label: 'LinkedIn', link: 'https://www.linkedin.com/in/cory-ginsberg/' },
+];
 
 export default function Header(): React.ReactElement {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const items = links.map(link => (
+    <Link
+      key={link.label}
+      href={link.link}
+      className={header.link}
+      onClick={event => {
+        event.preventDefault();
+      }}>
+      {link.label}
+    </Link>
+  ));
+
   return (
-    <div className={header.root}>
-      <div className={header.sectionLinks}>
-        <Link href="https://github.com/coryginsberg" className={header.link}>
-          GitHub
-        </Link>
-        <Link
-          href="https://www.linkedin.com/in/cory-ginsberg/"
-          className={header.link}>
-          LinkedIn
-        </Link>
-      </div>
-      <div className={header.logo}>
+    <header className={header.root}>
+      <Container size="xl" className={header.inner}>
+        <Group gap={8} visibleFrom="xs">
+          {items}
+        </Group>
         <Image
           src="/Logo.svg"
           alt="Personal Logo"
@@ -32,12 +48,13 @@ export default function Header(): React.ReactElement {
           width={60}
           height={60}
         />
-      </div>
-      <div className={header.buttonGroup}>
-        <Link href="mailto:cory.ginsberg1@gmail.com">
-          <RoundedButton>Contact me</RoundedButton>
-        </Link>
-      </div>
-    </div>
+        <Group className={header.buttonGroup}>
+          <Link href="mailto:cory.ginsberg1@gmail.com">
+            <RoundedButton>Contact me</RoundedButton>
+          </Link>
+        </Group>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+      </Container>
+    </header>
   );
 }
